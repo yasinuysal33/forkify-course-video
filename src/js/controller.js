1ad61f049283196471c9d4671521778490ebbb80
yasinuysal33 <uysal.e.yasin@gmail.com>
2022-02-21 22:24:50 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,6 +127,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
